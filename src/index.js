daa7e437d6f9bb0aca73a578b1811dcc3c855c61
Makanaka95 <MakanakaChitambo@gmail.com>
2024-08-18 23:18:42 +0200

    updated js and html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = searchInput.value;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "3f310a9d1446bt67o90cafe1b1ebbfbc";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+let searchInput;
+
 function clickSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  console.log(searchInput);
-  let cityElement = document.querySelector("#weather-city");
-  console.log(cityElement);
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", clickSearchSubmit);
+
+searchCity("Perth");
