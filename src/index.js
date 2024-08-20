a8b8e44f609613280b08c2c05d7265f290cfa3bf
Makanaka95 <MakanakaChitambo@gmail.com>
2024-08-20 09:56:18 +0200

    updated the date and time in Java Script

--- src/index.js
+++ src/index.js
@@ -8,12 +8,37 @@ function refreshWeather(response) {
   console.log(humidityElement);
   let windSpeedElement = document.querySelector("#wind-speed");
   console.log(windSpeedElement);
+  let timeElement = document.querySelector("#time");
+  console.log(timeElement);
+  let date = new Date(response.data.time * 1000);
+  console.log(date);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  console.log(day);
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
