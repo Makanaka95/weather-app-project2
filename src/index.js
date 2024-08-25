15896863aa1ea82d0a7cbea5e80a39c01a4fa698
Makanaka95 <MakanakaChitambo@gmail.com>
2024-08-25 22:12:18 +0200

    injected my forecast HTML in JS

--- src/index.js
+++ src/index.js
@@ -58,7 +58,34 @@ function clickSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  console.log(days);
+  let forecastHtml = "";
+  console.log(forecastHtml);
+
+  days.forEach(function (day) {
+    console.log(day);
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15°</strong>
+              </div>
+              <div class="weather-forecast-temperature">9°</div>
+            </div>
+          </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  console.log(forecastElement);
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", clickSearchSubmit);
 
 searchCity("Perth");
+displayForecast();
