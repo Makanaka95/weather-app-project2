0bacfdcfe92cf91976e5933c35d48fc5f3eabce0
Makanaka95 <MakanakaChitambo@gmail.com>
2024-08-27 00:07:20 +0200

    updated my JS forecast

--- src/index.js
+++ src/index.js
@@ -60,6 +60,15 @@ function clickSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  console.log(date);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  console.log(days);
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   console.log(city);
   let apiKey = "3f310a9d1446bt67o90cafe1b1ebbfbc";
@@ -71,25 +80,30 @@ function getForecast(city) {
 
 function displayForecast(response) {
   console.log(response.data);
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  console.log(days);
+
   let forecastHtml = "";
   console.log(forecastHtml);
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day, index) {
     console.log(day);
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌤️</div>
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <img src="${
+              day.condition.icon_url
+            }" class="weather-forecast-icon" />
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>15°</strong>
+                <strong>${Math.round(day.temperature.maximum)}°</strong>
               </div>
-              <div class="weather-forecast-temperature">9°</div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°</div>
             </div>
           </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   console.log(forecastElement);
